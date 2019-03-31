782ea3d678c0a2b65939509d60042ea2af13566c
jiangkuan <jiangkuan@chehejia.com>
2019-03-31 18:18:53 +0800

    操作列表

---
+++ 4_ chapter/course4.3_test.py
@@ -0,0 +1,18 @@
+for value in range(1,21):
+    print(value)
+numbers = [value for value in range(1,1000001)]
+#for number in numbers:
+#    print(number)
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+numbers = [value for value in range(1,20,2)]
+for number in numbers:
+    print(number)
+numbers = [value for value in range(3,31,3)]
+for number in numbers:
+    print(number)
+numbers = [value**3 for value in range(1,11)]
+for number in numbers:
+    print(number)
+    
---
+++ 4_ chapter/numbers.py
@@ -0,0 +1,7 @@
+for value in range(1,5):
+    print(value) 
+numbers = list(range(1,6))
+print(numbers)
+
+even_numbers = list(range(1,20,1))
+print(even_numbers)
